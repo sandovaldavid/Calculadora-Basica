dcf6488e389255527d92998c2c9462a5b4259569
sandovaldavid <sandovaldavid2201@gmai.com>
2023-06-30 17:02:51 -0500

    Termine de crear los botones

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,276 +27,305 @@ Partial Class Form1
         btnCE = New Button()
         btnC = New Button()
         btnDEL = New Button()
-        Button5 = New Button()
+        btnDivision = New Button()
         btnRaiz = New Button()
         btnCuadrado = New Button()
         btnInversa = New Button()
-        Button9 = New Button()
-        Button10 = New Button()
-        Button11 = New Button()
-        Button12 = New Button()
-        Button13 = New Button()
-        Button14 = New Button()
-        Button15 = New Button()
-        Button16 = New Button()
-        Button17 = New Button()
-        Button18 = New Button()
-        Button19 = New Button()
-        Button20 = New Button()
-        Button21 = New Button()
-        Button22 = New Button()
-        Button23 = New Button()
-        Button24 = New Button()
+        btnMultiplicacion = New Button()
+        btnNine = New Button()
+        btnEight = New Button()
+        btnSeven = New Button()
+        btnResta = New Button()
+        btnSix = New Button()
+        btnFive = New Button()
+        btnFour = New Button()
+        btnSuma = New Button()
+        btnThree = New Button()
+        btnTwo = New Button()
+        btnOne = New Button()
+        btnIgual = New Button()
+        btnPunto = New Button()
+        btnout = New Button()
+        btnCambioSigno = New Button()
         TextBox1 = New TextBox()
         SuspendLayout()
         ' 
         ' btnPorcentaje
         ' 
+        btnPorcentaje.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
         btnPorcentaje.Location = New Point(11, 83)
         btnPorcentaje.Name = "btnPorcentaje"
-        btnPorcentaje.Size = New Size(81, 51)
+        btnPorcentaje.Size = New Size(80, 51)
         btnPorcentaje.TabIndex = 0
         btnPorcentaje.Text = "%"
         btnPorcentaje.UseVisualStyleBackColor = True
         ' 
         ' btnCE
         ' 
+        btnCE.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
         btnCE.Location = New Point(98, 83)
         btnCE.Name = "btnCE"
-        btnCE.Size = New Size(81, 51)
+        btnCE.Size = New Size(80, 51)
         btnCE.TabIndex = 1
         btnCE.Text = "CE"
         btnCE.UseVisualStyleBackColor = True
         ' 
         ' btnC
         ' 
+        btnC.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
         btnC.Location = New Point(185, 83)
         btnC.Name = "btnC"
-        btnC.Size = New Size(81, 51)
+        btnC.Size = New Size(80, 51)
         btnC.TabIndex = 2
         btnC.Text = "C"
         btnC.UseVisualStyleBackColor = True
         ' 
         ' btnDEL
         ' 
+        btnDEL.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
         btnDEL.Location = New Point(272, 83)
         btnDEL.Name = "btnDEL"
-        btnDEL.Size = New Size(81, 51)
+        btnDEL.Size = New Size(80, 51)
         btnDEL.TabIndex = 3
         btnDEL.Text = "DEL"
         btnDEL.UseVisualStyleBackColor = True
         ' 
-        ' Button5
+        ' btnDivision
         ' 
-        Button5.Location = New Point(272, 140)
-        Button5.Name = "Button5"
-        Button5.Size = New Size(81, 51)
-        Button5.TabIndex = 7
-        Button5.Text = "Button5"
-        Button5.UseVisualStyleBackColor = True
+        btnDivision.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnDivision.Location = New Point(272, 140)
+        btnDivision.Name = "btnDivision"
+        btnDivision.Size = New Size(80, 51)
+        btnDivision.TabIndex = 7
+        btnDivision.Text = "∕"
+        btnDivision.UseVisualStyleBackColor = True
         ' 
         ' btnRaiz
         ' 
+        btnRaiz.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
         btnRaiz.Location = New Point(185, 140)
         btnRaiz.Name = "btnRaiz"
-        btnRaiz.Size = New Size(81, 51)
+        btnRaiz.Size = New Size(80, 51)
         btnRaiz.TabIndex = 6
         btnRaiz.Text = "√x"
         btnRaiz.UseVisualStyleBackColor = True
         ' 
         ' btnCuadrado
         ' 
+        btnCuadrado.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
         btnCuadrado.Location = New Point(98, 140)
         btnCuadrado.Name = "btnCuadrado"
-        btnCuadrado.Size = New Size(81, 51)
+        btnCuadrado.Size = New Size(80, 51)
         btnCuadrado.TabIndex = 5
         btnCuadrado.Text = "x²"
         btnCuadrado.UseVisualStyleBackColor = True
         ' 
         ' btnInversa
         ' 
+        btnInversa.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
         btnInversa.Location = New Point(11, 140)
         btnInversa.Name = "btnInversa"
-        btnInversa.Size = New Size(81, 51)
+        btnInversa.Size = New Size(80, 51)
         btnInversa.TabIndex = 4
         btnInversa.Text = "1/x"
         btnInversa.UseVisualStyleBackColor = True
         ' 
-        ' Button9
+        ' btnMultiplicacion
         ' 
-        Button9.Location = New Point(272, 197)
-        Button9.Name = "Button9"
-        Button9.Size = New Size(81, 51)
-        Button9.TabIndex = 11
-        Button9.Text = "Button9"
-        Button9.UseVisualStyleBackColor = True
+        btnMultiplicacion.Font = New Font("Cascadia Code", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        btnMultiplicacion.Location = New Point(272, 197)
+        btnMultiplicacion.Name = "btnMultiplicacion"
+        btnMultiplicacion.Size = New Size(80, 51)
+        btnMultiplicacion.TabIndex = 11
+        btnMultiplicacion.Text = "⨉"
+        btnMultiplicacion.UseVisualStyleBackColor = True
         ' 
-        ' Button10
+        ' btnNine
         ' 
-        Button10.Location = New Point(185, 197)
-        Button10.Name = "Button10"
-        Button10.Size = New Size(81, 51)
-        Button10.TabIndex = 10
-        Button10.Text = "Button10"
-        Button10.UseVisualStyleBackColor = True
+        btnNine.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnNine.Location = New Point(185, 197)
+        btnNine.Name = "btnNine"
+        btnNine.Size = New Size(80, 51)
+        btnNine.TabIndex = 10
+        btnNine.Text = "9"
+        btnNine.UseVisualStyleBackColor = True
         ' 
-        ' Button11
+        ' btnEight
         ' 
-        Button11.Location = New Point(98, 197)
-        Button11.Name = "Button11"
-        Button11.Size = New Size(81, 51)
-        Button11.TabIndex = 9
-        Button11.Text = "Button11"
-        Button11.UseVisualStyleBackColor = True
+        btnEight.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEight.Location = New Point(98, 196)
+        btnEight.Name = "btnEight"
+        btnEight.Size = New Size(80, 51)
+        btnEight.TabIndex = 9
+        btnEight.Text = "8"
+        btnEight.UseVisualStyleBackColor = True
         ' 
-        ' Button12
+        ' btnSeven
         ' 
-        Button12.Location = New Point(11, 197)
-        Button12.Name = "Button12"
-        Button12.Size = New Size(81, 51)
-        Button12.TabIndex = 8
-        Button12.Text = "Button12"
-        Button12.UseVisualStyleBackColor = True
+        btnSeven.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnSeven.Location = New Point(11, 197)
+        btnSeven.Name = "btnSeven"
+        btnSeven.Size = New Size(80, 51)
+        btnSeven.TabIndex = 8
+        btnSeven.Text = "7"
+        btnSeven.UseVisualStyleBackColor = True
         ' 
-        ' Button13
+        ' btnResta
         ' 
-        Button13.Location = New Point(271, 253)
-        Button13.Name = "Button13"
-        Button13.Size = New Size(81, 51)
-        Button13.TabIndex = 15
-        Button13.Text = "Button13"
-        Button13.UseVisualStyleBackColor = True
+        btnResta.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnResta.Location = New Point(271, 253)
+        btnResta.Name = "btnResta"
+        btnResta.Size = New Size(80, 51)
+        btnResta.TabIndex = 15
+        btnResta.Text = "-"
+        btnResta.UseVisualStyleBackColor = True
         ' 
-        ' Button14
+        ' btnSix
         ' 
-        Button14.Location = New Point(184, 253)
-        Button14.Name = "Button14"
-        Button14.Size = New Size(81, 51)
-        Button14.TabIndex = 14
-        Button14.Text = "Button14"
-        Button14.UseVisualStyleBackColor = True
+        btnSix.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnSix.Location = New Point(184, 253)
+        btnSix.Name = "btnSix"
+        btnSix.Size = New Size(80, 51)
+        btnSix.TabIndex = 14
+        btnSix.Text = "6"
+        btnSix.UseVisualStyleBackColor = True
         ' 
-        ' Button15
+        ' btnFive
         ' 
-        Button15.Location = New Point(97, 253)
-        Button15.Name = "Button15"
-        Button15.Size = New Size(81, 51)
-        Button15.TabIndex = 13
-        Button15.Text = "Button15"
-        Button15.UseVisualStyleBackColor = True
+        btnFive.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnFive.Location = New Point(98, 253)
+        btnFive.Name = "btnFive"
+        btnFive.Size = New Size(80, 51)
+        btnFive.TabIndex = 13
+        btnFive.Text = "5"
+        btnFive.UseVisualStyleBackColor = True
         ' 
-        ' Button16
+        ' btnFour
         ' 
-        Button16.Location = New Point(10, 253)
-        Button16.Name = "Button16"
-        Button16.Size = New Size(81, 51)
-        Button16.TabIndex = 12
-        Button16.Text = "Button16"
-        Button16.UseVisualStyleBackColor = True
+        btnFour.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnFour.Location = New Point(10, 253)
+        btnFour.Name = "btnFour"
+        btnFour.Size = New Size(80, 51)
+        btnFour.TabIndex = 12
+        btnFour.Text = "4"
+        btnFour.UseVisualStyleBackColor = True
         ' 
-        ' Button17
+        ' btnSuma
         ' 
-        Button17.Location = New Point(272, 310)
-        Button17.Name = "Button17"
-        Button17.Size = New Size(81, 51)
-        Button17.TabIndex = 19
-        Button17.Text = "Button17"
-        Button17.UseVisualStyleBackColor = True
+        btnSuma.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnSuma.Location = New Point(272, 310)
+        btnSuma.Name = "btnSuma"
+        btnSuma.Size = New Size(80, 51)
+        btnSuma.TabIndex = 19
+        btnSuma.Text = "+"
+        btnSuma.UseVisualStyleBackColor = True
         ' 
-        ' Button18
+        ' btnThree
         ' 
-        Button18.Location = New Point(185, 310)
-        Button18.Name = "Button18"
-        Button18.Size = New Size(81, 51)
-        Button18.TabIndex = 18
-        Button18.Text = "Button18"
-        Button18.UseVisualStyleBackColor = True
+        btnThree.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnThree.Location = New Point(185, 310)
+        btnThree.Name = "btnThree"
+        btnThree.Size = New Size(80, 51)
+        btnThree.TabIndex = 18
+        btnThree.Text = "3"
+        btnThree.UseVisualStyleBackColor = True
         ' 
-        ' Button19
+        ' btnTwo
         ' 
-        Button19.Location = New Point(98, 310)
-        Button19.Name = "Button19"
-        Button19.Size = New Size(81, 51)
-        Button19.TabIndex = 17
-        Button19.Text = "Button19"
-        Button19.UseVisualStyleBackColor = True
+        btnTwo.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnTwo.Location = New Point(98, 310)
+        btnTwo.Name = "btnTwo"
+        btnTwo.Size = New Size(80, 51)
+        btnTwo.TabIndex = 17
+        btnTwo.Text = "2"
+        btnTwo.UseVisualStyleBackColor = True
         ' 
-        ' Button20
+        ' btnOne
         ' 
-        Button20.Location = New Point(11, 310)
-        Button20.Name = "Button20"
-        Button20.Size = New Size(81, 51)
-        Button20.TabIndex = 16
-        Button20.Text = "Button20"
-        Button20.UseVisualStyleBackColor = True
+        btnOne.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnOne.Location = New Point(11, 310)
+        btnOne.Name = "btnOne"
+        btnOne.Size = New Size(80, 51)
+        btnOne.TabIndex = 16
+        btnOne.Text = "1"
+        btnOne.UseVisualStyleBackColor = True
         ' 
-        ' Button21
+        ' btnIgual
         ' 
-        Button21.Location = New Point(272, 367)
-        Button21.Name = "Button21"
-        Button21.Size = New Size(81, 51)
-        Button21.TabIndex = 23
-        Button21.Text = "Button21"
-        Button21.UseVisualStyleBackColor = True
+        btnIgual.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnIgual.Location = New Point(272, 367)
+        btnIgual.Name = "btnIgual"
+        btnIgual.Size = New Size(80, 51)
+        btnIgual.TabIndex = 23
+        btnIgual.Text = "="
+        btnIgual.UseVisualStyleBackColor = True
         ' 
-        ' Button22
+        ' btnPunto
         ' 
-        Button22.Location = New Point(185, 367)
-        Button22.Name = "Button22"
-        Button22.Size = New Size(81, 51)
-        Button22.TabIndex = 22
-        Button22.Text = "Button22"
-        Button22.UseVisualStyleBackColor = True
+        btnPunto.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnPunto.Location = New Point(185, 367)
+        btnPunto.Name = "btnPunto"
+        btnPunto.Size = New Size(80, 51)
+        btnPunto.TabIndex = 22
+        btnPunto.Text = "."
+        btnPunto.UseVisualStyleBackColor = True
         ' 
-        ' Button23
+        ' btnout
         ' 
-        Button23.Location = New Point(98, 367)
-        Button23.Name = "Button23"
-        Button23.Size = New Size(81, 51)
-        Button23.TabIndex = 21
-        Button23.Text = "Button23"
-        Button23.UseVisualStyleBackColor = True
+        btnout.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnout.Location = New Point(98, 367)
+        btnout.Name = "btnout"
+        btnout.Size = New Size(80, 51)
+        btnout.TabIndex = 21
+        btnout.Text = "0"
+        btnout.UseVisualStyleBackColor = True
         ' 
-        ' Button24
+        ' btnCambioSigno
         ' 
-        Button24.Location = New Point(11, 367)
-        Button24.Name = "Button24"
-        Button24.Size = New Size(81, 51)
-        Button24.TabIndex = 20
-        Button24.Text = "Button24"
-        Button24.UseVisualStyleBackColor = True
+        btnCambioSigno.Font = New Font("Cascadia Code", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        btnCambioSigno.Location = New Point(11, 367)
+        btnCambioSigno.Name = "btnCambioSigno"
+        btnCambioSigno.Size = New Size(80, 51)
+        btnCambioSigno.TabIndex = 20
+        btnCambioSigno.Text = "+/_"
+        btnCambioSigno.UseVisualStyleBackColor = True
         ' 
         ' TextBox1
         ' 
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Cursor = Cursors.IBeam
+        TextBox1.Font = New Font("Cascadia Code", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
         TextBox1.Location = New Point(10, 40)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(342, 27)
         TextBox1.TabIndex = 25
+        TextBox1.Text = "0"
+        TextBox1.TextAlign = HorizontalAlignment.Right
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.ButtonHighlight
-        ClientSize = New Size(365, 426)
+        ClientSize = New Size(365, 439)
         Controls.Add(TextBox1)
-        Controls.Add(Button21)
-        Controls.Add(Button22)
-        Controls.Add(Button23)
-        Controls.Add(Button24)
-        Controls.Add(Button17)
-        Controls.Add(Button18)
-        Controls.Add(Button19)
-        Controls.Add(Button20)
-        Controls.Add(Button13)
-        Controls.Add(Button14)
-        Controls.Add(Button15)
-        Controls.Add(Button16)
-        Controls.Add(Button9)
-        Controls.Add(Button10)
-        Controls.Add(Button11)
-        Controls.Add(Button12)
-        Controls.Add(Button5)
+        Controls.Add(btnIgual)
+        Controls.Add(btnPunto)
+        Controls.Add(btnout)
+        Controls.Add(btnCambioSigno)
+        Controls.Add(btnSuma)
+        Controls.Add(btnThree)
+        Controls.Add(btnTwo)
+        Controls.Add(btnOne)
+        Controls.Add(btnResta)
+        Controls.Add(btnSix)
+        Controls.Add(btnFive)
+        Controls.Add(btnFour)
+        Controls.Add(btnMultiplicacion)
+        Controls.Add(btnNine)
+        Controls.Add(btnEight)
+        Controls.Add(btnSeven)
+        Controls.Add(btnDivision)
         Controls.Add(btnRaiz)
         Controls.Add(btnCuadrado)
         Controls.Add(btnInversa)
@@ -317,25 +346,25 @@ Partial Class Form1
     Friend WithEvents btnCE As Button
     Friend WithEvents btnC As Button
     Friend WithEvents btnDEL As Button
-    Friend WithEvents Button5 As Button
+    Friend WithEvents btnDivision As Button
     Friend WithEvents btnRaiz As Button
     Friend WithEvents btnCuadrado As Button
     Friend WithEvents btnInversa As Button
-    Friend WithEvents Button9 As Button
-    Friend WithEvents Button10 As Button
-    Friend WithEvents Button11 As Button
-    Friend WithEvents Button12 As Button
-    Friend WithEvents Button13 As Button
-    Friend WithEvents Button14 As Button
-    Friend WithEvents Button15 As Button
-    Friend WithEvents Button16 As Button
-    Friend WithEvents Button17 As Button
-    Friend WithEvents Button18 As Button
-    Friend WithEvents Button19 As Button
-    Friend WithEvents Button20 As Button
-    Friend WithEvents Button21 As Button
-    Friend WithEvents Button22 As Button
-    Friend WithEvents Button23 As Button
-    Friend WithEvents Button24 As Button
+    Friend WithEvents btnMultiplicacion As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnSeven As Button
+    Friend WithEvents btnResta As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnSuma As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnIgual As Button
+    Friend WithEvents btnPunto As Button
+    Friend WithEvents btnout As Button
+    Friend WithEvents btnCambioSigno As Button
     Friend WithEvents TextBox1 As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,5 @@
 ﻿Public Class Form1
-    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles btnFour.Click
 
     End Sub
 
@@ -12,6 +8,98 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPorcentaje.Click
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles btnEight.Click
+
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnCE_Click(sender As Object, e As EventArgs) Handles btnCE.Click
+
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+
+    End Sub
+
+    Private Sub btnDEL_Click(sender As Object, e As EventArgs) Handles btnDEL.Click
+
+    End Sub
+
+    Private Sub btnInversa_Click(sender As Object, e As EventArgs) Handles btnInversa.Click
+
+    End Sub
+
+    Private Sub btnCuadrado_Click(sender As Object, e As EventArgs) Handles btnCuadrado.Click
+
+    End Sub
+
+    Private Sub btnRaiz_Click(sender As Object, e As EventArgs) Handles btnRaiz.Click
+
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+
+    End Sub
+
+    Private Sub btnSeven_Click(sender As Object, e As EventArgs) Handles btnSeven.Click
+
+    End Sub
+
+    Private Sub btnNine_Click(sender As Object, e As EventArgs) Handles btnNine.Click
+
+    End Sub
+
+    Private Sub btnMultiplicacion_Click(sender As Object, e As EventArgs) Handles btnMultiplicacion.Click
+
+    End Sub
+
+    Private Sub btnFive_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+
+    End Sub
+
+    Private Sub btnSix_Click(sender As Object, e As EventArgs) Handles btnSix.Click
+
+    End Sub
+
+    Private Sub btnResta_Click(sender As Object, e As EventArgs) Handles btnResta.Click
+
+    End Sub
+
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+
+    End Sub
+
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+
+    End Sub
+
+    Private Sub btnSuma_Click(sender As Object, e As EventArgs) Handles btnSuma.Click
+
+    End Sub
+
+    Private Sub btnCambioSigno_Click(sender As Object, e As EventArgs) Handles btnCambioSigno.Click
+
+    End Sub
+
+    Private Sub btnout_Click(sender As Object, e As EventArgs) Handles btnout.Click
+
+    End Sub
+
+    Private Sub btnPunto_Click(sender As Object, e As EventArgs) Handles btnPunto.Click
 
     End Sub
 End Class
