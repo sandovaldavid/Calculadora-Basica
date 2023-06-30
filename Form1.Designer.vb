4159aa4c85c12bbddedaa767a4497292d0e6e0a3
sandovaldavid <sandovaldavid2201@gmai.com>
2023-06-30 16:39:34 -0500

    Inicio

---
+++ Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnPorcentaje = New Button()
+        btnCE = New Button()
+        btnC = New Button()
+        btnDEL = New Button()
+        Button5 = New Button()
+        btnRaiz = New Button()
+        btnCuadrado = New Button()
+        btnInversa = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        Button21 = New Button()
+        Button22 = New Button()
+        Button23 = New Button()
+        Button24 = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnPorcentaje
+        ' 
+        btnPorcentaje.Location = New Point(11, 83)
+        btnPorcentaje.Name = "btnPorcentaje"
+        btnPorcentaje.Size = New Size(81, 51)
+        btnPorcentaje.TabIndex = 0
+        btnPorcentaje.Text = "%"
+        btnPorcentaje.UseVisualStyleBackColor = True
+        ' 
+        ' btnCE
+        ' 
+        btnCE.Location = New Point(98, 83)
+        btnCE.Name = "btnCE"
+        btnCE.Size = New Size(81, 51)
+        btnCE.TabIndex = 1
+        btnCE.Text = "CE"
+        btnCE.UseVisualStyleBackColor = True
+        ' 
+        ' btnC
+        ' 
+        btnC.Location = New Point(185, 83)
+        btnC.Name = "btnC"
+        btnC.Size = New Size(81, 51)
+        btnC.TabIndex = 2
+        btnC.Text = "C"
+        btnC.UseVisualStyleBackColor = True
+        ' 
+        ' btnDEL
+        ' 
+        btnDEL.Location = New Point(272, 83)
+        btnDEL.Name = "btnDEL"
+        btnDEL.Size = New Size(81, 51)
+        btnDEL.TabIndex = 3
+        btnDEL.Text = "DEL"
+        btnDEL.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(272, 140)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(81, 51)
+        Button5.TabIndex = 7
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' btnRaiz
+        ' 
+        btnRaiz.Location = New Point(185, 140)
+        btnRaiz.Name = "btnRaiz"
+        btnRaiz.Size = New Size(81, 51)
+        btnRaiz.TabIndex = 6
+        btnRaiz.Text = "√x"
+        btnRaiz.UseVisualStyleBackColor = True
+        ' 
+        ' btnCuadrado
+        ' 
+        btnCuadrado.Location = New Point(98, 140)
+        btnCuadrado.Name = "btnCuadrado"
+        btnCuadrado.Size = New Size(81, 51)
+        btnCuadrado.TabIndex = 5
+        btnCuadrado.Text = "x²"
+        btnCuadrado.UseVisualStyleBackColor = True
+        ' 
+        ' btnInversa
+        ' 
+        btnInversa.Location = New Point(11, 140)
+        btnInversa.Name = "btnInversa"
+        btnInversa.Size = New Size(81, 51)
+        btnInversa.TabIndex = 4
+        btnInversa.Text = "1/x"
+        btnInversa.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(272, 197)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(81, 51)
+        Button9.TabIndex = 11
+        Button9.Text = "Button9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(185, 197)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(81, 51)
+        Button10.TabIndex = 10
+        Button10.Text = "Button10"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(98, 197)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(81, 51)
+        Button11.TabIndex = 9
+        Button11.Text = "Button11"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(11, 197)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(81, 51)
+        Button12.TabIndex = 8
+        Button12.Text = "Button12"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' Button13
+        ' 
+        Button13.Location = New Point(271, 253)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(81, 51)
+        Button13.TabIndex = 15
+        Button13.Text = "Button13"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' Button14
+        ' 
+        Button14.Location = New Point(184, 253)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(81, 51)
+        Button14.TabIndex = 14
+        Button14.Text = "Button14"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Button15
+        ' 
+        Button15.Location = New Point(97, 253)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(81, 51)
+        Button15.TabIndex = 13
+        Button15.Text = "Button15"
+        Button15.UseVisualStyleBackColor = True
+        ' 
+        ' Button16
+        ' 
+        Button16.Location = New Point(10, 253)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(81, 51)
+        Button16.TabIndex = 12
+        Button16.Text = "Button16"
+        Button16.UseVisualStyleBackColor = True
+        ' 
+        ' Button17
+        ' 
+        Button17.Location = New Point(272, 310)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(81, 51)
+        Button17.TabIndex = 19
+        Button17.Text = "Button17"
+        Button17.UseVisualStyleBackColor = True
+        ' 
+        ' Button18
+        ' 
+        Button18.Location = New Point(185, 310)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(81, 51)
+        Button18.TabIndex = 18
+        Button18.Text = "Button18"
+        Button18.UseVisualStyleBackColor = True
+        ' 
+        ' Button19
+        ' 
+        Button19.Location = New Point(98, 310)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(81, 51)
+        Button19.TabIndex = 17
+        Button19.Text = "Button19"
+        Button19.UseVisualStyleBackColor = True
+        ' 
+        ' Button20
+        ' 
+        Button20.Location = New Point(11, 310)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(81, 51)
+        Button20.TabIndex = 16
+        Button20.Text = "Button20"
+        Button20.UseVisualStyleBackColor = True
+        ' 
+        ' Button21
+        ' 
+        Button21.Location = New Point(272, 367)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(81, 51)
+        Button21.TabIndex = 23
+        Button21.Text = "Button21"
+        Button21.UseVisualStyleBackColor = True
+        ' 
+        ' Button22
+        ' 
+        Button22.Location = New Point(185, 367)
+        Button22.Name = "Button22"
+        Button22.Size = New Size(81, 51)
+        Button22.TabIndex = 22
+        Button22.Text = "Button22"
+        Button22.UseVisualStyleBackColor = True
+        ' 
+        ' Button23
+        ' 
+        Button23.Location = New Point(98, 367)
+        Button23.Name = "Button23"
+        Button23.Size = New Size(81, 51)
+        Button23.TabIndex = 21
+        Button23.Text = "Button23"
+        Button23.UseVisualStyleBackColor = True
+        ' 
+        ' Button24
+        ' 
+        Button24.Location = New Point(11, 367)
+        Button24.Name = "Button24"
+        Button24.Size = New Size(81, 51)
+        Button24.TabIndex = 20
+        Button24.Text = "Button24"
+        Button24.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(10, 40)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(342, 27)
+        TextBox1.TabIndex = 25
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ButtonHighlight
+        ClientSize = New Size(365, 426)
+        Controls.Add(TextBox1)
+        Controls.Add(Button21)
+        Controls.Add(Button22)
+        Controls.Add(Button23)
+        Controls.Add(Button24)
+        Controls.Add(Button17)
+        Controls.Add(Button18)
+        Controls.Add(Button19)
+        Controls.Add(Button20)
+        Controls.Add(Button13)
+        Controls.Add(Button14)
+        Controls.Add(Button15)
+        Controls.Add(Button16)
+        Controls.Add(Button9)
+        Controls.Add(Button10)
+        Controls.Add(Button11)
+        Controls.Add(Button12)
+        Controls.Add(Button5)
+        Controls.Add(btnRaiz)
+        Controls.Add(btnCuadrado)
+        Controls.Add(btnInversa)
+        Controls.Add(btnDEL)
+        Controls.Add(btnC)
+        Controls.Add(btnCE)
+        Controls.Add(btnPorcentaje)
+        HelpButton = True
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Calculadora"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnPorcentaje As Button
+    Friend WithEvents btnCE As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnDEL As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents btnRaiz As Button
+    Friend WithEvents btnCuadrado As Button
+    Friend WithEvents btnInversa As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnPorcentaje.Click
+
+    End Sub
+End Class
